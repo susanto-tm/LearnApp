c6ef3f7656afa0c310adaaf147feb8384ab4910f
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-07-11 22:15:51 +0700

    TestFixture iOS activated

--- LearnApp.AutomatedUITests/Tests.cs
+++ LearnApp.AutomatedUITests/Tests.cs
@@ -8,7 +8,7 @@ using Xamarin.UITest.Queries;
 namespace LearnApp.AutomatedUITests
 {
 	[TestFixture(Platform.Android)]
-	//[TestFixture(Platform.iOS)]
+	[TestFixture(Platform.iOS)]
 	public class Tests
 	{
 		IApp app;
